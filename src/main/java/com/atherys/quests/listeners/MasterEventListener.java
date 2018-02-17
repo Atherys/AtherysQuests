bbc17ad761f6e8f8153550210c564a89edfe1e21
Haed Hutner <mvvasilev@outlook.com>
2018-02-17 16:28:02 +0200

    Trying to fix exceptions at startup. Changed @First annotation on master event listener with manual Optional check

--- src/main/java/com/atherys/quests/listeners/MasterEventListener.java
+++ src/main/java/com/atherys/quests/listeners/MasterEventListener.java
@@ -4,13 +4,12 @@ import com.atherys.quests.managers.QuesterManager;
 import org.spongepowered.api.entity.living.player.Player;
 import org.spongepowered.api.event.Event;
 import org.spongepowered.api.event.Listener;
-import org.spongepowered.api.event.filter.cause.First;
 
 public class MasterEventListener {
 
     @Listener
-    public void onEvent ( Event event, @First Player player ) {
-        QuesterManager.getInstance().notify ( event, player );
+    public void onEvent ( Event event ) {
+        event.getCause().first(Player.class).ifPresent(player -> QuesterManager.getInstance().notify(event, player));
     }
 
 }
