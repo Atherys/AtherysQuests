85a5f58ad998ef4c9a2e3c973dad508da02231da
HaedHutner <mvvasilev@outlook.com>
2018-03-03 14:18:51 +0200

    Annotated interfaces as ConfigSerializable... again...

--- src/main/java/com/atherys/quests/quest/reward/Reward.java
+++ src/main/java/com/atherys/quests/quest/reward/Reward.java
@@ -3,7 +3,9 @@ package com.atherys.quests.quest.reward;
 import com.atherys.quests.base.Prototype;
 import com.atherys.quests.quester.Quester;
 import com.atherys.quests.views.TextViewable;
+import ninja.leaping.configurate.objectmapping.serialize.ConfigSerializable;
 
+@ConfigSerializable
 public interface Reward extends Prototype<Reward>, TextViewable {
 
     boolean award ( Quester quester );
