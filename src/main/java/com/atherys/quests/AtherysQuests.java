441f3d95887cd987d97179abc0def99d54b7c994
Haed Hutner <mvvasilev@outlook.com>
2018-03-25 18:19:44 +0300

    Fixing DialogData

--- src/main/java/com/atherys/quests/AtherysQuests.java
+++ src/main/java/com/atherys/quests/AtherysQuests.java
@@ -226,9 +226,10 @@ public class AtherysQuests {
 
     @Listener
     public void testListener ( DamageEntityEvent event ) {
-        logger.info( Sponge.getCauseStackManager().popCause().getClass().getName() );
         Optional<AtherysDamageType> first = event.getCause().first( AtherysDamageType.class );
         first.ifPresent( type -> logger.info( "Detected damage with A'therys Type: " + type.getName() ) );
+
+        logger.info( Sponge.getCauseStackManager().popCause().getClass().getName() );
     }
 
     @Listener
