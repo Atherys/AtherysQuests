38c39e95d96097b3b1ff4c2fccae36ccb320e684
LJNIC <liamjnicolle@gmail.com>
2018-05-08 20:46:12 -0300

    Finish /quest tolocation

--- src/main/java/com/atherys/quests/commands/quest/AttachQuestToLocationCommand.java
+++ src/main/java/com/atherys/quests/commands/quest/AttachQuestToLocationCommand.java
@@ -1,30 +1,43 @@
 package com.atherys.quests.commands.quest;
 
+import com.atherys.core.command.ParameterizedCommand;
+import com.atherys.core.command.annotation.Aliases;
+import com.atherys.core.command.annotation.Description;
+import com.atherys.quests.managers.LocationManager;
 import org.spongepowered.api.command.CommandException;
 import org.spongepowered.api.command.CommandResult;
 import org.spongepowered.api.command.CommandSource;
 import org.spongepowered.api.command.args.CommandContext;
+import org.spongepowered.api.command.args.CommandElement;
 import org.spongepowered.api.command.args.GenericArguments;
 import org.spongepowered.api.command.spec.CommandExecutor;
 import org.spongepowered.api.command.spec.CommandSpec;
 import org.spongepowered.api.entity.living.player.Player;
 import org.spongepowered.api.text.Text;
 
+import javax.annotation.Nonnull;
+import java.util.Optional;
 
-public class AttachQuestToLocationCommand implements CommandExecutor {
+@Aliases("tolocation")
+@Description("Attaches a quest to a location.")
+public class AttachQuestToLocationCommand implements CommandExecutor, ParameterizedCommand {
+
+    @Nonnull
     @Override
     public CommandResult execute(CommandSource src, CommandContext args) throws CommandException {
         if(!(src instanceof Player)) return CommandResult.empty();
+
+        Optional<Player> player = ((Player) src).getPlayer();
+        if(player.isPresent() && args.getOne(Text.of("questId")).isPresent() && args.getOne(Text.of("radius")).isPresent())
+            LocationManager.getInstance().addLocationQuest(player.get().getLocation(), args.<String>getOne("questId").get(), args.<Double>getOne(Text.of("radius")).get());
         return CommandResult.success();
-
-
     }
 
-    public CommandSpec getCommandSpec() {
-        return CommandSpec.builder()
-                .executor(this)
-                .arguments(GenericArguments.string(Text.of("dialogId")))
-                .arguments(GenericArguments.doubleNum(Text.of("radius")))
-                .build();
+    @Override
+    public CommandElement[] getArguments() {
+        return new CommandElement[]{
+                GenericArguments.string(Text.of("questId")),
+                GenericArguments.doubleNum(Text.of("radius"))
+        };
     }
 }
--- src/main/java/com/atherys/quests/managers/LocationManager.java
+++ src/main/java/com/atherys/quests/managers/LocationManager.java
@@ -5,22 +5,47 @@ import com.atherys.core.database.mongo.AbstractMongoDatabaseManager;
 import com.atherys.quests.AtherysQuests;
 import com.atherys.quests.api.quest.Quest;
 import com.atherys.quests.db.QuestsDatabase;
+import com.flowpowered.math.vector.Vector3d;
 import org.bson.Document;
 import org.spongepowered.api.world.Location;
 import org.spongepowered.api.world.World;
 
+import java.util.Map;
 import java.util.Optional;
 import java.util.UUID;
 
 public final class LocationManager extends AbstractMongoDatabaseManager<LocationManager.QuestLocation> {
 
-    protected LocationManager() {
+    private static LocationManager instance = new LocationManager();
+
+    private LocationManager() {
         super(AtherysQuests.getInstance().getLogger(), QuestsDatabase.getInstance(), "questLocations");
     }
 
     @Override
     protected Optional<Document> toDocument(QuestLocation questLocation) {
         return Optional.empty();
+    }
+
+    public Optional<QuestLocation> getByLocation(Location<World> location){
+        for(QuestLocation loc : getCache().values()){
+            if(loc.contains(location)){
+                return Optional.of(loc);
+            }
+        }
+        return Optional.empty();
+    }
+
+    public static LocationManager getInstance(){
+        return instance;
+    }
+
+    public void addLocationQuest(Location<World> location, String questId, double radius){
+        QuestManager.getInstance().getQuest(questId).ifPresent(quest -> {
+            QuestLocation questLocation = new QuestLocation(location, quest, radius);
+            this.save(questLocation);
+        });
+
     }
 
     @Override
@@ -37,6 +62,12 @@ public final class LocationManager extends AbstractMongoDatabaseManager<Location
 
         private String questId;
 
+        private boolean contains(Location<World> loc){
+            if(loc.getExtent().equals(location.getExtent())){
+                return(loc.getPosition().distance(location.getPosition()) <= radius);
+            } else return false;
+        }
+
         private QuestLocation(Location<World> location, Quest quest, double radius) {
             this.uuid = UUID.randomUUID();
             this.location = location;
@@ -44,6 +75,9 @@ public final class LocationManager extends AbstractMongoDatabaseManager<Location
             this.questId = quest.getId();
         }
 
+        public String getQuestId(){
+            return questId;
+        }
         @Override
         public UUID getUUID() {
             return uuid;
