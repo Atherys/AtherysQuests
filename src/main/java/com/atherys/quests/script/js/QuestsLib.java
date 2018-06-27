f4eba37b618c4adb19ed04d6216bb2b988570e8a
Haed Hutner <mvvasilev@outlook.com>
2018-06-27 22:53:40 +0300

    Forgot to push this

--- src/main/java/com/atherys/quests/script/js/QuestsLib.java
+++ src/main/java/com/atherys/quests/script/js/QuestsLib.java
@@ -1,8 +1,11 @@
 package com.atherys.quests.script.js;
 
+import com.atherys.quests.AtherysQuests;
+import com.atherys.quests.script.js.lib.block.BlockFunctions;
 import com.atherys.quests.script.js.lib.item.ItemStackFunctions;
 import com.atherys.quests.script.js.lib.location.LocationFunctions;
 import com.atherys.quests.script.js.lib.player.PlayerFunctions;
+import com.atherys.quests.script.js.lib.task.TaskFunctions;
 import com.atherys.quests.script.js.lib.text.TextFunctions;
 import com.atherys.quests.script.js.lib.util.UtilityFunctions;
 
@@ -22,7 +25,11 @@ public final class QuestsLib {
         new TextFunctions().put(engine);
         new PlayerFunctions().put(engine);
         new LocationFunctions().put(engine);
+        new TaskFunctions().put(engine);
+        new BlockFunctions().put(engine);
         new UtilityFunctions().put(engine);
+
+        engine.put("QUESTS_PLUGIN", AtherysQuests.getInstance());
     }
 
     public ScriptEngine getEngine() {
