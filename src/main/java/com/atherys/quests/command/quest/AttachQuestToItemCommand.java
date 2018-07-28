7a76ad21aaaa149b740d23120e351d3e33b9165c
Liam <liamreadsspam@gmail.com>
2018-07-28 15:33:54 -0300

    Permissions on /quest toitem

--- src/main/java/com/atherys/quests/command/quest/AttachQuestToItemCommand.java
+++ src/main/java/com/atherys/quests/command/quest/AttachQuestToItemCommand.java
@@ -3,6 +3,7 @@ package com.atherys.quests.command.quest;
 import com.atherys.core.command.ParameterizedCommand;
 import com.atherys.core.command.annotation.Aliases;
 import com.atherys.core.command.annotation.Description;
+import com.atherys.core.command.annotation.Permission;
 import com.atherys.quests.api.quest.Quest;
 import com.atherys.quests.data.QuestData;
 import com.atherys.quests.service.QuestService;
@@ -21,6 +22,7 @@ import org.spongepowered.api.text.Text;
 import javax.annotation.Nonnull;
 import java.util.Optional;
 
+@Permission("atherysquests.admin.quest.toitem")
 @Aliases("toitem")
 @Description("Sets a quest to the item in hand.")
 public class AttachQuestToItemCommand implements ParameterizedCommand {
