26531784538196fce044d51a48cf93a78ee7766d
miroslav.vasilev <miroslav.vasilev@scalefocus.com>
2018-07-30 14:16:33 +0300

    Added all the new functions to the Quest library extension

--- src/main/java/com/atherys/quests/script/lib/QuestExtension.java
+++ src/main/java/com/atherys/quests/script/lib/QuestExtension.java
@@ -5,8 +5,14 @@ import com.atherys.quests.event.dialog.DialogProceedEvent;
 import com.atherys.quests.event.dialog.DialogRegistrationEvent;
 import com.atherys.quests.event.dialog.DialogStartEvent;
 import com.atherys.quests.event.quest.*;
-import com.atherys.quests.script.lib.quest.AddQuestObjectives;
-import com.atherys.quests.script.lib.quest.CreateSimpleQuest;
+import com.atherys.quests.script.lib.dialog.RegisterDialogTree;
+import com.atherys.quests.script.lib.objective.DialogObjectiveFunc;
+import com.atherys.quests.script.lib.objective.InteractWithBlockFunc;
+import com.atherys.quests.script.lib.objective.KillEntityObjectiveFunc;
+import com.atherys.quests.script.lib.objective.ReachLocationObjectiveFunc;
+import com.atherys.quests.script.lib.quest.*;
+import com.atherys.quests.script.lib.requriement.*;
+import com.atherys.quests.script.lib.reward.SingleItemRewardFunc;
 import com.atherys.script.api.library.LibraryExtension;
 import com.atherys.script.api.library.ScriptLibrary;
 import com.atherys.script.js.library.event.EventHandlerFunction;
@@ -33,7 +39,35 @@ public class QuestExtension implements LibraryExtension {
         scriptLibrary.put("onQuestTurnIn", new EventHandlerFunction<>(QuestTurnedInEvent.class));
 
         scriptLibrary.put("createSimpleQuest", new CreateSimpleQuest());
+        scriptLibrary.put("createStagedQuest", new CreateStagedQuest());
+
+        scriptLibrary.put("addQuestStages", new AddQuestStages());
         scriptLibrary.put("addQuestObjectives", new AddQuestObjectives());
+        scriptLibrary.put("addQuestRequirements", new AddQuestRequirements());
+        scriptLibrary.put("addQuestRewards", new AddQuestRewards());
+
+        scriptLibrary.put("getQuestStages", new GetQuestStages());
+        scriptLibrary.put("getQuestObjectives", new GetQuestObjectives());
+        scriptLibrary.put("getQuestRequirements", new GetQuestRequirements());
+        scriptLibrary.put("getQuestRewards", new GetQuestRewards());
+
+        scriptLibrary.put("dialogObjective", new DialogObjectiveFunc());
+        scriptLibrary.put("interactWithBlockObjective", new InteractWithBlockFunc());
+        scriptLibrary.put("killEntityObjective", new KillEntityObjectiveFunc());
+        scriptLibrary.put("reachLocationObjective", new ReachLocationObjectiveFunc());
+
+        scriptLibrary.put("andRequirement", new AndRequirementFunc());
+        scriptLibrary.put("orRequirement", new OrRequirementFunc());
+        scriptLibrary.put("notRequirement", new NotRequirementFunc());
+        scriptLibrary.put("questRequirement", new QuestRequirementFunc());
+        scriptLibrary.put("moneyRequirement", new MoneyRequirementFunc());
+        scriptLibrary.put("levelRequirement", new LevelRequirementFunc());
+
+        scriptLibrary.put("moneyReward", new MoneyRequirementFunc());
+        scriptLibrary.put("singleItemReward", new SingleItemRewardFunc());
+
+        scriptLibrary.put("registerQuest", new RegisterQuest());
+        scriptLibrary.put("registerDialogTree", new RegisterDialogTree());
     }
 
     public static QuestExtension getInstance() {
